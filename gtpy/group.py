61f473225e7d1704db9722031dbbb237bba25f9c
Narek Mouse <narek.mouse@gmail.com>
2018-03-12 23:22:05 +0400

    add space

--- gtpy/group.py
+++ gtpy/group.py
@@ -8,9 +8,11 @@ class AbstractElement(ABC):
     def get_invers(self):
         pass
 
+
 class AdditiveElement(AbstractElement):
     def get_invers(self):
         return 0-self.value
+
 
 class MultiplicativeElement(AbstractElement):
     def get_invers(self):
