3d0c9931568a7a5b620a8eb282ac8f7a6b2e45e0
Narek Mouse <narek.mouse@gmail.com>
2018-03-13 19:25:22 +0400

    add Abstract Group class

--- gtpy/group.py
+++ gtpy/group.py
@@ -1,6 +1,10 @@
 from abc import ABC, abstractmethod
 
 class AbstractElement(ABC):
+    """
+    This is an sample of element
+    from
+    """
     def __init__(self,value):
         self.value = value
 
@@ -8,6 +12,19 @@ class AbstractElement(ABC):
     def get_invers(self):
         pass
 
+class AbstractGroup(ABC):
+    def __init__(self,elements):
+        self.elements = elements
+        self.__complete()
+
+    def __complete(self):
+        for element in self.elements:
+            if not element.get_invers() in self.elements:
+                self.elements.add(element.get_invers())
+
+    def add_element(self,element):
+        self.elements.add(element)
+        self.__complete()
 
 class AdditiveElement(AbstractElement):
     def get_invers(self):
