4d66f42bb0804837d048a14d7fe4216683c57c71
narekmouse <narek.mouse@gmail.com>
2018-04-28 11:16:09 +0400

    update setup

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup
 
+from setuptools import find_packages
+
 setup(name='MagicCubeLib',
       version='1.0-Druid',
       description='Magic Cube library | Designed to solve the problem of the Rubik\'s cube',
       author='Narek Mouse',
       author_email='narek.mouse@gmail.com',
       url='https://github.com/Narekmouse/MagicCubeLib',
-      packages=['MCState','MCState.MCSide','MCView','MCStep' ],
+      packages=find_packages(),
      )
